cecad2c8bd29c69cb97594e698c21e09f8cd32a6
Eric K Wu <ekuan.wu@gmail.com>
2018-04-25 10:30:10 -0700

    Hook up reactotron

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import RootContainer from './Containers/RootContainer';
 import createStore from './Redux';
+import './Config/ReactotronConfig';
+import Reactotron from 'reactotron-react-native';
+import RootContainer from './Containers/RootContainer';
 
 const store = createStore();
 
--- Redux/CreateStore.js
+++ Redux/CreateStore.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import Reactotron from 'reactotron-react-native';
 import thunk from 'redux-thunk';
 
 // creates the store
@@ -16,7 +17,7 @@ export default rootReducer => {
 
   enhancers.push(applyMiddleware(...middleware));
 
-  const store = createStore(rootReducer, compose(...enhancers));
+  const store = Reactotron.createStore(rootReducer, compose(...enhancers));
 
   return store;
 };
