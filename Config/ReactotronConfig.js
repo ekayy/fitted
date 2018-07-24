34a91e24862b13fe8e7dc16b26591f80b24b3b85
Eric K Wu <ekuan.wu@gmail.com>
2018-07-24 12:37:36 -0700

    Add console.tron debug statement

--- Config/ReactotronConfig.js
+++ Config/ReactotronConfig.js
@@ -5,3 +5,7 @@ Reactotron.configure() // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .use(reactotronRedux())
   .connect();
+
+// Totally hacky, but this allows you to not both importing reactotron-react-native
+// on every file.  This is just DEV mode, so no big deal.
+console.tron = Reactotron;
