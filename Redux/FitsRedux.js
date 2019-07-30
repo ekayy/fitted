062e7cb420528ded8cb670d1495046f34bb8a85a
Eric K Wu <ekuan.wu@gmail.com>
2019-07-29 17:40:44 -0700

    Tagging and removing garments from fit in FitsRedux

--- Redux/FitsRedux.js
+++ Redux/FitsRedux.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 import { baseURL } from '../Config';
 
 // Actions
-const FETCH_FITS_BEGIN = 'FETCH_FITS_BEGIN';
-const FETCH_FITS_SUCCESS = 'FETCH_FITS_SUCCESS';
-const FETCH_FITS_FAILURE = 'FETCH_FITS_FAILURE';
+// const CREATE_FIT_BEGIN = 'CREATE_FIT_BEGIN';
+// const CREATE_FIT_SUCCESS = 'CREATE_FIT_SUCCESS';
+// const CREATE_FIT_FAILURE = 'CREATE_FIT_FAILURE';
+const TAG_GARMENT_TO_FIT = 'TAG_GARMENT_TO_FIT';
+const REMOVE_GARMENT_FROM_FIT = 'REMOVE_GARMENT_FROM_FIT';
 
 export const INITIAL_STATE = {
-  items: [],
+  garments: [],
   loading: false,
   error: null
 };
@@ -15,54 +17,77 @@ export const INITIAL_STATE = {
 // Reducer
 export default function fits(state = INITIAL_STATE, action = {}) {
   switch (action.type) {
-    case FETCH_FITS_BEGIN:
+    // case CREATE_FIT_BEGIN:
+    //   return {
+    //     ...state,
+    //     loading: true,
+    //     error: null
+    //   };
+    // case CREATE_FIT_SUCCESS:
+    //   return {
+    //     ...state,
+    //     loading: false,
+    //     items: [...state.items, ...action.payload.fits]
+    //   };
+
+    // case CREATE_FIT_FAILURE:
+    //   return {
+    //     ...state,
+    //     loading: false,
+    //     error: action.payload.error,
+    //     items
+    //   };
+
+    case TAG_GARMENT_TO_FIT:
       return {
         ...state,
-        loading: true,
-        error: null
-      };
-    case FETCH_FITS_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        items: [...state.items, ...action.payload.fits]
+        garments: [...state.garments, action.payload.garmentId]
       };
 
-    case FETCH_FITS_FAILURE:
+    case REMOVE_GARMENT_FROM_FIT:
       return {
         ...state,
-        loading: false,
-        error: action.payload.error,
-        items
+        garments: state.garments.filter(id => id !== action.payload.garmentId)
       };
+
     default:
       return state;
   }
 }
 
 // Action Creators
-export const fetchFitsBegin = () => ({
-  type: FETCH_FITS_BEGIN
+// export const createFitBegin = () => ({
+//   type: CREATE_FIT_BEGIN
+// });
+
+// export const createFitSuccess = fits => ({
+//   type: CREATE_FIT_SUCCESS,
+//   payload: { fits }
+// });
+
+// export const createFitFailure = error => ({
+//   type: CREATE_FIT_FAILURE,
+//   payload: { error }
+// });
+
+export const tagGarmentToFit = garmentId => ({
+  type: TAG_GARMENT_TO_FIT,
+  payload: { garmentId }
 });
 
-export const fetchFitsSuccess = fits => ({
-  type: FETCH_FITS_SUCCESS,
-  payload: { fits }
-});
-
-export const fetchFitsFailure = error => ({
-  type: FETCH_FITS_FAILURE,
-  payload: { error }
+export const removeGarmentFromFit = garmentId => ({
+  type: REMOVE_GARMENT_FROM_FIT,
+  payload: { garmentId }
 });
 
 // side effects, only as applicable
 // e.g. thunks, epics, etc
-export function fetchFits(page) {
-  return dispatch => {
-    dispatch(fetchFitsBegin());
-    return axios
-      .get(`${baseURL}/fits/?page=${page}`)
-      .then(response => dispatch(fetchFitsSuccess(response.data.results)))
-      .catch(error => dispatch(fetchFitsFailure(error)));
-  };
-}
+// export function fetchFits(page) {
+//   return dispatch => {
+//     dispatch(fetchFitsBegin());
+//     return axios
+//       .get(`${baseURL}/fits/?page=${page}`)
+//       .then(response => dispatch(fetchFitsSuccess(response.data.results)))
+//       .catch(error => dispatch(fetchFitsFailure(error)));
+//   };
+// }
