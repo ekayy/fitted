34a348884a6e9c20f8d42361a72441d65fca175e
Eric K Wu <ekuan.wu@gmail.com>
2019-09-19 16:13:05 -0700

    Fits page camera button should start camera with associated garment

--- Containers/Fits.js
+++ Containers/Fits.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
 import FitsGrid from '../Components/FitsGrid';
 import { Ionicons } from '@expo/vector-icons';
+import { tagGarmentToFit, clearCreatedFit } from '../Redux/FitsRedux';
 
 class Fits extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -33,9 +35,18 @@ class Fits extends Component {
     });
   }
 
-  _navigateCamera = () => {
+  _navigateCamera = async () => {
+    const { navigate } = this.props.navigation;
+    const { clearCreatedFit, tagGarmentToFit } = this.props;
+    const { currentGarment } = this.props.navigation.state.params;
+
     this.setState({ error: null });
-    this.props.navigation.navigate('Camera');
+
+    console.tron.log(currentGarment);
+
+    await clearCreatedFit();
+    await tagGarmentToFit(currentGarment);
+    navigate('Camera');
   };
 
   render() {
@@ -64,4 +75,11 @@ const styles = {
   }
 };
 
-export default Fits;
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  { tagGarmentToFit, clearCreatedFit }
+)(Fits);
--- Containers/GarmentDetail.js
+++ Containers/GarmentDetail.js
@@ -112,13 +112,6 @@ class GarmentDetail extends Component {
     navigate('Camera');
   };
 
-  viewFitPhotos = () => {
-    const { navigate } = this.props.navigation;
-    const { fits } = this.state;
-
-    navigate('Fits', { fits });
-  };
-
   render() {
     const {
       id,
@@ -128,8 +121,10 @@ class GarmentDetail extends Component {
       model,
       photo
     } = this.props.navigation.state.params;
+    const currentGarment = this.props.navigation.state.params;
+    const { navigate } = this.props.navigation;
 
-    const { refreshing, count } = this.state;
+    const { refreshing, count, fits } = this.state;
 
     return (
       <ScrollView style={styles.container}>
@@ -229,7 +224,7 @@ class GarmentDetail extends Component {
               title={`See all ${count} photos`}
               buttonStyle={[AppStyles.buttonAltStyle]}
               titleStyle={AppStyles.buttonAltTitleStyle}
-              onPress={this.viewFitPhotos}
+              onPress={() => navigate('Fits', { fits, currentGarment })}
             />
           </View>
         </View>
@@ -289,6 +284,7 @@ class GarmentDetail extends Component {
               title="See all 2,900 comments"
               buttonStyle={[AppStyles.buttonAltStyle]}
               titleStyle={AppStyles.buttonAltTitleStyle}
+              onPress={() => navigate('Comments')}
             />
           </View>
         </View>
