aab1119d179902f0f4b01dfdb50fa69969e3c332
Eric K Wu <ekuan.wu@gmail.com>
2019-10-22 17:25:03 -0700

    Add discussions filter

--- Containers/Comments.js
+++ Containers/Comments.js
@@ -6,12 +6,14 @@ import {
   Text,
   View,
   ScrollView,
-  Modal
+  Modal,
+  Picker
 } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { AppStyles } from '../Themes';
 import CommentSingle from '../Components/Comment/CommentSingle';
 import CommentInput from '../Components/Comment/CommentInput';
+import DropDown from '../Components/DropDown';
 
 const Comments = ({ navigation }) => {
   const { currentGarment, contentType } = navigation.state.params;
@@ -29,6 +31,8 @@ const Comments = ({ navigation }) => {
   const [showModal, setModal] = useState(false);
   const [currentComment, setCurrentComment] = useState({});
   const [searchedComments, setSearchedComments] = useState(comments);
+  const [tag, setTag] = useState('');
+  const [showFilters, setShowFilter] = useState(false);
 
   useEffect(() => {
     navigation.setParams({
@@ -63,7 +67,7 @@ const Comments = ({ navigation }) => {
 
   return (
     <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
-      <ScrollView>
+      <ScrollView style={{ flex: 1 }}>
         <View>
           <SearchBar
             containerStyle={styles.searchBarContainer}
@@ -77,6 +81,23 @@ const Comments = ({ navigation }) => {
             platform="ios"
             value={searchValue}
           />
+
+          <StyledFilterBarContainer>
+            <StyledFilterBar>
+              <DropDown
+                options={['MOST RECENT', 'MOST POPULAR']}
+                defaultValue="SELECT"
+              />
+            </StyledFilterBar>
+
+            <VerticalDivider />
+
+            <StyledFilterBar>
+              <StyledFilterButton onPress={() => setShowFilter(!showFilters)}>
+                <StyledFilterText>FILTER</StyledFilterText>
+              </StyledFilterButton>
+            </StyledFilterBar>
+          </StyledFilterBarContainer>
 
           <View style={AppStyles.section}>
             <View style={AppStyles.sectionTitle}>
@@ -117,6 +138,27 @@ const Comments = ({ navigation }) => {
           contentType={contentType}
         />
       </Modal>
+
+      {showFilters && (
+        <StyledPickerContainer>
+          <StyledPickerHeader>
+            <StyledPickerDone onPress={() => setShowFilter(false)}>
+              Done
+            </StyledPickerDone>
+          </StyledPickerHeader>
+          <StyledPicker
+            selectedValue={tag}
+            onValueChange={(itemValue, itemIndex) => setTag(itemValue)}
+            itemStyle={{ color: '#fff' }}
+          >
+            <Picker.Item label="All" value="all" />
+            <Picker.Item label="Care" value="care" />
+            <Picker.Item label="Styling" value="styling" />
+            <Picker.Item label="Sizing" value="sizing" />
+            <Picker.Item label="Other" value="other" />
+          </StyledPicker>
+        </StyledPickerContainer>
+      )}
     </KeyboardAvoidingView>
   );
 };
@@ -132,6 +174,47 @@ Comments.navigationOptions = ({ navigation }) => ({
 const StyledHeaderButton = styled.TouchableOpacity`
   margin-right: 20px;
 `;
+const StyledFilterBarContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+`;
+const StyledFilterBar = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledFilterButton = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+`;
+const StyledFilterText = styled.Text`
+  text-align: center;
+`;
+const VerticalDivider = styled.View`
+  border-left-width: 1;
+  align-self: stretch;
+`;
+
+const StyledPickerContainer = styled.View`
+  flex: 1;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.9);
+`;
+const StyledPickerHeader = styled.View`
+  justify-content: center;
+  align-items: flex-end;
+  padding: 20px 20px 0 20px;
+`;
+const StyledPickerDone = styled.Text`
+  color: #fff;
+`;
+const StyledPicker = styled.Picker``;
 
 const styles = {
   searchBarContainer: {
