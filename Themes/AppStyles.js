30538514b80f1447e037f5798f2ffa41cc123aa2
Eric K Wu <ekuan.wu@gmail.com>
2020-06-08 00:35:01 -0700

    Stylesheet type error fix

--- Themes/AppStyles.js
+++ Themes/AppStyles.js
@@ -1,59 +1,36 @@
 import Fonts from './Fonts';
 import Metrics from './Metrics';
 import Colors from './Colors';
+import { StyleSheet } from 'react-native';
 
 // This file is for a reusable grouping of Theme items.
 // Similar to an XML fragment layout in Android
 
-const AppStyles = {
-  screen: {
-    section: {
-      margin: Metrics.section,
-      padding: Metrics.baseMargin
-    },
-    sectionText: {
-      ...Fonts.style.normal,
-      paddingVertical: Metrics.doubleBaseMargin,
-      color: Colors.snow,
-      marginVertical: Metrics.smallMargin,
-      textAlign: 'center'
-    },
-    subtitle: {
-      color: Colors.snow,
-      padding: Metrics.smallMargin,
-      marginBottom: Metrics.smallMargin,
-      marginHorizontal: Metrics.smallMargin
-    },
-    titleText: {
-      ...Fonts.style.h2,
-      fontSize: 14,
-      color: Colors.text
-    }
-  },
+const AppStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.snow
+    backgroundColor: Colors.snow,
   },
 
   section: {
     display: 'flex',
-    padding: Metrics.baseMargin
+    padding: Metrics.baseMargin,
   },
   sectionTitle: {
     display: 'flex',
     paddingBottom: Metrics.smallMargin,
     marginBottom: Metrics.smallMargin,
     borderBottomWidth: 1,
-    borderBottomColor: Colors.border
+    borderBottomColor: Colors.border,
   },
 
   sectionTitleText: {
-    color: '#333'
+    color: '#333',
   },
   sectionSubtitle: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
 
   buttonAltStyle: {
@@ -61,33 +38,33 @@ const AppStyles = {
     borderWidth: 1,
     borderColor: '#444',
     paddingVertical: Metrics.baseMargin,
-    width: '100%'
+    width: '100%',
   },
   buttonAltTitleStyle: {
     color: '#444',
-    fontSize: 13
+    fontSize: 13,
   },
   buttonDefaultStyle: {
     backgroundColor: '#444',
     borderWidth: 1,
     borderColor: '#444',
     paddingVertical: Metrics.baseMargin,
-    width: '100%'
+    width: '100%',
   },
   button: {
     display: 'flex',
     width: '100%',
-    paddingVertical: Metrics.doubleBaseMargin
+    paddingVertical: Metrics.doubleBaseMargin,
   },
 
   form: {
     margin: Metrics.baseMargin,
     borderRadius: 5,
-    width: 315
+    width: 315,
   },
   loginRow: {
     paddingVertical: Metrics.doubleBaseMargin,
-    paddingHorizontal: Metrics.doubleBaseMargin
+    paddingHorizontal: Metrics.doubleBaseMargin,
   },
   formRow: {
     backgroundColor: Colors.snow,
@@ -96,26 +73,26 @@ const AppStyles = {
     borderWidth: 0.5,
     borderColor: 'rgb(236,236,237)',
     height: 50.5,
-    paddingVertical: 5
+    paddingVertical: 5,
   },
   rowLabel: {
-    color: Colors.charcoal
+    color: Colors.charcoal,
   },
   textInput: {
     height: 40,
     color: Colors.coal,
-    fontSize: 17
+    fontSize: 17,
   },
   textInputReadonly: {
     height: 40,
-    color: Colors.steel
+    color: Colors.steel,
   },
 
   error: {
     color: 'red',
     flex: 1,
-    textAlign: 'center'
-  }
-};
+    textAlign: 'center',
+  },
+});
 
 export default AppStyles;
--- Themes/Colors.js
+++ Themes/Colors.js
@@ -1,3 +1,5 @@
+import { StyleSheet } from 'react-native';
+
 const colors = {
   background: '#1F0808',
   clear: 'rgba(0,0,0,0)',
@@ -8,7 +10,7 @@ const colors = {
   border: '#eee',
   text: '#E0D7E5',
   dark: 'rgba(68, 68, 68, 1)',
-  darkFade: 'rgba(68, 68, 68, 0.8)'
+  darkFade: 'rgba(68, 68, 68, 0.8)',
 };
 
 export default colors;
