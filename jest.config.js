544e64c883c6502416c3180e1432a35f468e0619
Eric K Wu <ekuan.wu@gmail.com>
2020-06-12 11:59:53 -0700

    Replace testing library

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,10 @@ module.exports = {
   transform: {
     ...tsJestConfig.transform,
   },
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: [
+    '@testing-library/react-native/cleanup-after-each',
+    '@testing-library/jest-native/extend-expect',
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
